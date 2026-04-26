05457207a5f2f5fe0add0ca7d515fc30489f3c1d
Eduardo Kern Goes da Silva <edugoes215@gmail.com>
2026-04-26 20:52:03 -0300

    Ajuste linguagem do código e time zone

--- storeapp/settings.py
+++ storeapp/settings.py
@@ -84,8 +84,6 @@ WSGI_APPLICATION = 'storeapp.wsgi.application'
 #    }
 #}
 
-
-#----------------------------------------------------------------
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.oracle',
@@ -118,9 +116,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
